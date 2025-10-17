23144bd6dc161d2bcf4a82921781080d53b2e2bb
Dylan Locksley <dylan011235@gmail.com>
2025-10-17 15:34:09 -0300

    fix exercise 11.9

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './e2e-tests',
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:8080',
+    // eslint-disable-next-line no-undef
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000 // wait up to 2 minutes for the server
+  },
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'http://localhost:8080/'
   }
