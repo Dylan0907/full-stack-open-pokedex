96a61ac7ba838d0209ef87a9b6d18fbcb7b69be9
Dylan Locksley <dylan011235@gmail.com>
2025-10-17 19:24:58 -0300

    Add webpack-cli for production build

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import path from 'path'
 const app = express()
+
 
 // get the port from env variable
 // eslint-disable-next-line no-undef
@@ -7,7 +9,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('dist', 'index.html'))
+})
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
+
+
