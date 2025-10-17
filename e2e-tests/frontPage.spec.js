5385028c4e35860aa0dda8ac479378fd293b2179
Dylan Locksley <dylan011235@gmail.com>
2025-10-17 15:50:18 -0300

    add e2e test for exercise 11.9

--- e2e-tests/frontPage.spec.js
+++ e2e-tests/frontPage.spec.js
@@ -10,4 +10,11 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur', { exact: true }).click()
+    await expect(page).toHaveURL(/.*ivysaur/i)
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
