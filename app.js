daa538d20e258f7527e850ad74e059fa2e3f7df9
Dylan Locksley <dylan011235@gmail.com>
2025-10-18 16:07:52 -0300

    add health check

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.get('/version', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('dist', 'index.html'))
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
